a2797e59d323070d2e350ad45bca59982cdf400f
Andrey P <prih@prihhost.com>
2015-12-10 02:24:16 +0200

    news modal

--- js/appNewsModule.js
+++ js/appNewsModule.js
@@ -33,6 +33,20 @@ angular.module('appNewsModule', ['ngRoute', 'appNavModule', 'appAPIModule'])
 			appAPIService.getNewsDel($scope.news);
 			break;
 	}
+
+	$scope.modal = {
+		title: '',
+		visible: false,
+		body: ''
+	};
+
+	$scope.openNewsModal = function($event, item_key){
+		$scope.modal.title = $scope.news.items[item_key].title;
+		$scope.modal.body = $scope.news.items[item_key].content;
+		$scope.modal.visible = true;
+
+		$event.preventDefault();
+	};
 }])
 
 .filter('url', function(){
@@ -41,5 +55,40 @@ angular.module('appNewsModule', ['ngRoute', 'appNavModule', 'appAPIModule'])
 	return function(input, key){
 		a.href = input;
 		return a[key];
+	};
+})
+
+.directive('modal', function(){
+	return {
+		templateUrl: 'js/views/news_modal.html',
+		restrict: 'E',
+		transclude: true,
+		scope: true,
+		replace: true,
+		link: function(scope, element, attrs){
+			scope.$watch(attrs.title, function(value){
+				scope.title = value;
+			});
+
+			scope.$watch(attrs.visible, function(value){
+				if(value){
+					$(element).modal('show');
+				} else {
+					$(element).modal('hide');
+				}
+			});
+
+			$(element).on('shown.bs.modal', function(){
+				scope.$apply(function(){
+					scope.$parent.$eval(attrs.visible + ' = true');
+				});
+			});
+
+			$(element).on('hidden.bs.modal', function(){
+				scope.$apply(function(){
+					scope.$parent.$eval(attrs.visible + ' = false');
+				});
+			});
+		}
 	};
 });
